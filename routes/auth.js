4730a3ba5b83089a6ef5e8f1dbe9b4ab40dd3148
= <mclaughlinambrose@gmail.com>
2023-12-17 17:50:57 -0500

    Added Logout to Dashboar

--- routes/auth.js
+++ routes/auth.js
@@ -13,4 +13,13 @@ router.get('/google/callback',passport.authenticate('google',{failureRedirect:'/
     res.redirect('/dashboard')
 } )
 
+
+//@desc Logout User
+// @route ///logout
+
+router.get('/logout',(req,res) =>{
+    req.logout()
+    res.redirect('/')
+})
+
 module.exports = router;
